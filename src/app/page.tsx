de124573f46faa027b7a891a49dd52aa26b2ac63
C.S.Harshitha <harshitha.cslv@gmail.com>
2026-09-16 01:11:11 +0530

    Add live 360 room scan entrypoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import OverhaulIntake from "@/components/overhaul/OverhaulIntakeV2";
 import DemoLauncher from "@/components/overhaul/DemoLauncher";
+import RoomScanOverlay from "@/components/overhaul/RoomScanOverlay";
 
 export default function HomePage() {
-  return <><OverhaulIntake /><DemoLauncher /></>;
+  return <><OverhaulIntake /><DemoLauncher /><RoomScanOverlay /></>;
 }
