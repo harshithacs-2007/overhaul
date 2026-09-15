6ad739be039edc9e903354502f87b0396d88323a
C.S.Harshitha <harshitha.cslv@gmail.com>
2026-09-16 03:44:45 +0530

    Reframe home as OVERHAUL welcome and inspection entry

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import OverhaulIntake from "@/components/overhaul/OverhaulIntakeV2";
-import DemoLauncher from "@/components/overhaul/DemoLauncher";
 
 export default function HomePage() {
-  return <><OverhaulIntake /><DemoLauncher /></>;
+  return <OverhaulIntake />;
 }
