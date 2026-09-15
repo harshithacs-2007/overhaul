d71619c9dee35fdb7b5a39672e40900df968928d
C.S.Harshitha <harshitha.cslv@gmail.com>
2026-09-16 00:38:46 +0530

    Wire corrected universal workspace into assessment route

---
+++ src/app/assessment/page.jsx
@@ -0,0 +1,5 @@
+import UniversalDecisionWorkspaceV2 from "@/components/overhaul/UniversalDecisionWorkspaceV2";
+
+export default function AssessmentPage() {
+  return <UniversalDecisionWorkspaceV2 />;
+}
