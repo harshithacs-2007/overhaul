cb98f214d204553dcc75dbb68f23009a72c5cd04
C.S.Harshitha <harshitha.cslv@gmail.com>
2026-09-16 01:49:56 +0530

    feat: expose final report from assessment

--- src/app/assessment/page.jsx
+++ src/app/assessment/page.jsx
@@ -1,5 +1,11 @@
 import AssessmentExperience from "@/components/overhaul/AssessmentExperience";
+import AssessmentReportLauncher from "@/components/overhaul/AssessmentReportLauncher";
 
 export default function AssessmentPage() {
-  return <AssessmentExperience />;
+  return (
+    <>
+      <AssessmentExperience />
+      <AssessmentReportLauncher />
+    </>
+  );
 }
