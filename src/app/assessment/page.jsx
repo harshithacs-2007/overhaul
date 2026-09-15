f6bbdf3255797b53388e68913ddeb0eeac80645e
C.S.Harshitha <harshitha.cslv@gmail.com>
2026-09-16 02:47:43 +0530

    feat: surface engineering closure layer in assessment

--- src/app/assessment/page.jsx
+++ src/app/assessment/page.jsx
@@ -1,12 +1,19 @@
 import AssessmentExperience from "@/components/overhaul/AssessmentExperience";
 import AssessmentReportLauncher from "@/components/overhaul/AssessmentReportLauncher";
 import RegionalClimateLens from "@/components/overhaul/RegionalClimateLens";
+import RetrofitIntelligenceSuite from "@/components/overhaul/RetrofitIntelligenceSuite";
 
 export default function AssessmentPage() {
   return (
     <>
       <AssessmentExperience />
       <RegionalClimateLens />
+      <RetrofitIntelligenceSuite
+        scope="building"
+        values={{}}
+        extracts={[]}
+        climate={null}
+      />
       <AssessmentReportLauncher />
     </>
   );
