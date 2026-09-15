4fc68d752ab066bdc3eb3f59d7224404ba3213d0
C.S.Harshitha <harshitha.cslv@gmail.com>
2026-09-16 00:55:22 +0530

    Add presentation-mode demo launcher to intake

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import OverhaulIntake from "@/components/overhaul/OverhaulIntakeV2";
+import DemoLauncher from "@/components/overhaul/DemoLauncher";
 
 export default function HomePage() {
-  return <OverhaulIntake />;
+  return <><OverhaulIntake /><DemoLauncher /></>;
 }
