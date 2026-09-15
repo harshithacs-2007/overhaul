627c4a9dbbf79fe072f4cc3ad9f18019b635e0d6
C.S.Harshitha <harshitha.cslv@gmail.com>
2026-09-16 01:50:31 +0530

    feat: surface regional climate context in assessment

--- src/app/assessment/page.jsx
+++ src/app/assessment/page.jsx
@@ -1,10 +1,12 @@
 import AssessmentExperience from "@/components/overhaul/AssessmentExperience";
 import AssessmentReportLauncher from "@/components/overhaul/AssessmentReportLauncher";
+import RegionalClimateLens from "@/components/overhaul/RegionalClimateLens";
 
 export default function AssessmentPage() {
   return (
     <>
       <AssessmentExperience />
+      <RegionalClimateLens />
       <AssessmentReportLauncher />
     </>
   );
