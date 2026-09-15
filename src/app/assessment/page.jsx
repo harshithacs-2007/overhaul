60622394c625fcbf8adecfe0d207bb889c78fd51
C.S.Harshitha <harshitha.cslv@gmail.com>
2026-09-16 02:54:19 +0530

    fix: remove duplicate retrofit runtime mount

--- src/app/assessment/page.jsx
+++ src/app/assessment/page.jsx
@@ -1,14 +1,12 @@
 import AssessmentExperience from "@/components/overhaul/AssessmentExperience";
 import AssessmentReportLauncher from "@/components/overhaul/AssessmentReportLauncher";
 import RegionalClimateLens from "@/components/overhaul/RegionalClimateLens";
-import RetrofitIntelligenceRuntime from "@/components/overhaul/RetrofitIntelligenceRuntime";
 
 export default function AssessmentPage() {
   return (
     <>
       <AssessmentExperience />
       <RegionalClimateLens />
-      <RetrofitIntelligenceRuntime />
       <AssessmentReportLauncher />
     </>
   );
