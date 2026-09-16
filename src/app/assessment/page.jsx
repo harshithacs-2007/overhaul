b3f3c1680a16a2c7592c36d0595f4d12a5841984
C.S.Harshitha <harshitha.cslv@gmail.com>
2026-09-16 08:57:25 +0530

    feat: surface animated retrofit counterfactual on assessment

--- src/app/assessment/page.jsx
+++ src/app/assessment/page.jsx
@@ -2,11 +2,13 @@ import AssessmentExperience from "@/components/overhaul/AssessmentExperience";
 import AssessmentReportLauncher from "@/components/overhaul/AssessmentReportLauncher";
 import RegionalClimateLens from "@/components/overhaul/RegionalClimateLens";
 import RoomScanCamera from "@/components/overhaul/RoomScanCamera";
+import RetrofitTransitionVisualizer from "@/components/overhaul/RetrofitTransitionVisualizer";
 
 export default function AssessmentPage() {
   return (
     <>
       <AssessmentExperience />
+      <RetrofitTransitionVisualizer />
       <RoomScanCamera />
       <RegionalClimateLens />
       <AssessmentReportLauncher />
