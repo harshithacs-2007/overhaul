471a45b9688c62be940abe452128a8e970056f73
C.S.Harshitha <harshitha.cslv@gmail.com>
2026-09-16 08:52:41 +0530

    fix: avoid duplicate retrofit CAD panel mounting

--- src/app/assessment/page.jsx
+++ src/app/assessment/page.jsx
@@ -2,14 +2,12 @@ import AssessmentExperience from "@/components/overhaul/AssessmentExperience";
 import AssessmentReportLauncher from "@/components/overhaul/AssessmentReportLauncher";
 import RegionalClimateLens from "@/components/overhaul/RegionalClimateLens";
 import RoomScanCamera from "@/components/overhaul/RoomScanCamera";
-import RetrofitCADPanel from "@/components/overhaul/RetrofitCADPanel";
 
 export default function AssessmentPage() {
   return (
     <>
       <AssessmentExperience />
       <RoomScanCamera />
-      <RetrofitCADPanel />
       <RegionalClimateLens />
       <AssessmentReportLauncher />
     </>
