24600e8c3d1c09fab834349b662099ee61f386e0
C.S.Harshitha <harshitha.cslv@gmail.com>
2026-09-16 08:38:43 +0530

    feat: surface live camera and retrofit CAD workspaces

--- src/app/assessment/page.jsx
+++ src/app/assessment/page.jsx
@@ -1,11 +1,13 @@
 import AssessmentExperience from "@/components/overhaul/AssessmentExperience";
 import AssessmentReportLauncher from "@/components/overhaul/AssessmentReportLauncher";
 import RegionalClimateLens from "@/components/overhaul/RegionalClimateLens";
+import RoomScanCamera from "@/components/overhaul/RoomScanCamera";
 
 export default function AssessmentPage() {
   return (
     <>
       <AssessmentExperience />
+      <RoomScanCamera />
       <RegionalClimateLens />
       <AssessmentReportLauncher />
     </>
